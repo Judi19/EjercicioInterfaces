46d5eba3ab56b649e7e3da2e744b3b6c8113e305
Judi19 <167385372+Judi19@users.noreply.github.com>
2024-11-09 19:48:00 -0600

    Agregar archivos de proyecto.

---
+++ EjercicioInterfaces/IOperaciones.cs
@@ -0,0 +1,11 @@
+﻿namespace EjercicioInterfaces
+{
+    public class IOperaciones
+    {
+        double AreaCuadrado(double lado);
+        double AreaRectangulo(double largo, double ancho);
+        double PerimetroCuadrado(double lado);
+        double PerimetroRectangulo(double largo, double ancho);
+        double VolumeCuadrado(double lado);
+    }
+}
---
+++ EjercicioInterfaces/Program.cs
@@ -0,0 +1,13 @@
+﻿using EjercicioInterfaces;
+
+Operaciones operaciones = new Operaciones();
+Console.WriteLine("Ingrese el lado del cuadrado: ");
+double ladoCuadrado = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ingrese el largo del rectangulo: ");
+double largoRectangulo = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ingrese ancho del rectaangulo: ");
+double anchoRectangulo = Convert.ToDouble(Console.ReadLine());
+
+operaciones.Imprimir(ladoCuadrado, largoRectangulo, anchoRectangulo);
